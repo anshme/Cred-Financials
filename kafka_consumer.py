68bc3125003573179ab08ceee6de003a4798f796
anshuman.a <anshuman.a@perfios.com>
2024-03-09 16:07:25 +0530

    Kafka conf setting

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -5,7 +5,10 @@ def kafka_consumer(kafka_conf):
     conf = {
         'bootstrap.servers': kafka_conf['bootstrap_server'],  # Kafka broker address
         'group.id': kafka_conf['group_id'],        # Consumer group ID
-        'auto.offset.reset': kafka_conf['offset_reset']         # Start from the earliest message available
+        'auto.offset.reset': kafka_conf['offset_reset'],
+        'enable.auto.commit': True,
+        'auto.commit.interval.ms': 1000,
+        'poll.interval.ms': 100
     }
 
     consumer = Consumer(conf)
