c97f06db925be3c9a8039ec48900227fe8f7cc73
anshuman.a <anshuman.a@perfios.com>
2024-03-09 16:41:43 +0530

    Kafka conf setting

--- driver.py
+++ driver.py
@@ -50,7 +50,8 @@ def execute():
 
     consumer = kafka_consumer(conf['kafka'])
     for msg in consumer:
-        print(msg.value)
+        print(type(msg.value), type(msg))
+        # print(json.loads(msg.value))
         # last_postcode, credit_score, txn_time, ucl = get_details_from_last_txn(hbase_connection,
         #                                                                        incoming_msg['card_id'],
         #                                                                        "lookup")
